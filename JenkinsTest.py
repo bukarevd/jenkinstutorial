2695ecaaa5e3aee24cd03acc10a4b920c1cb4872
bukarevd <bukarevd@JENKINS.cniim.local>
2019-08-14 15:18:25 +0300

    Test jenkins with github

---
+++ JenkinsTest.py
@@ -0,0 +1,11 @@
+class JenkinsTest():
+        def __init__(self, words):
+            self.words = words
+
+        def printing(self):
+            print(self.words)
+
+
+test_ex = JenkinsTest("Hello world")
+test_ex.printing()
+
